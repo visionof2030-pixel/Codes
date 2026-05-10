747ec67878ea644f965fddf33e8bc72a13d0e56e
visionof2030-pixel <visionof2030@gmail.com>
2026-05-10 10:24:14 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+
+  res.send("Code Nexus Backend Running");
+
+});
+
+app.listen(PORT, () => {
+
+  console.log(`Server running on port ${PORT}`);
+
+});
