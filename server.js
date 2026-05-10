969a43911a363882ba7958e581e385882d7cbf6c
visionof2030-pixel <visionof2030@gmail.com>
2026-05-10 10:58:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const fs = require("fs");
 
 const path = require("path");
 
+const cors = require("cors");
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -11,6 +13,8 @@ const PORT = process.env.PORT || 3000;
 // ======================
 // Middleware
 // ======================
+
+app.use(cors());
 
 app.use(express.json());
 
